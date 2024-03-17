c08b0f91a1050ecc860777c3ad461ea6963d07aa
abdelrhman <115238439+abdelrhman892@users.noreply.github.com>
2024-03-17 17:38:10 +0200

    updating the models for sql

--- models/state.py
+++ models/state.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+# State Module for HBNB project
+from sqlalchemy import Column, String
+
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    # State class
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
